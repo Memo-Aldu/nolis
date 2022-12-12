bb29563daf4d876b98317761811ede55f389e517
Memo- Aldu <71087733+Memo-Aldu@users.noreply.github.com>
2022-12-11 20:41:18 -0500

    got the search-registry service connected to nolis

--- nolisservices/search-registry/src/main/java/com/nolis/searchregistry/controller/SearchRegistryController.java
+++ nolisservices/search-registry/src/main/java/com/nolis/searchregistry/controller/SearchRegistryController.java
@@ -1,17 +1,26 @@
 package com.nolis.searchregistry.controller;
 
+import com.nolis.commonconfig.security.service.AuthService;
+import com.nolis.searchregistry.helper.ControllerHelper;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import javax.servlet.http.HttpServletRequest;
+
+import static org.springframework.http.HttpHeaders.AUTHORIZATION;
 
 @Slf4j
 @RestController
 @RequestMapping("api/v1/search-registry")
-public record SearchRegistryController() {
+public record SearchRegistryController(
+        ControllerHelper controllerHelper
+) {
 
-    @GetMapping("/")
-    public String test() {
+    @PostMapping("/amazon/{asin}")
+    public String saveAmazonProductSearchWithAsin(@PathVariable String asin, HttpServletRequest request) {
+        if(controllerHelper.hasAuthority(request,"ROLE_BESTBUY_USER")){
+            return "Amazon product with asin: " + asin + " saved";
+        }
         return "test";
     }
 }
---
+++ nolisservices/search-registry/src/main/java/com/nolis/searchregistry/helper/ControllerHelper.java
@@ -0,0 +1,30 @@
+package com.nolis.searchregistry.helper;
+
+
+import com.nolis.commonconfig.security.service.AuthService;
+import lombok.AllArgsConstructor;
+import org.springframework.http.HttpHeaders;
+import org.springframework.stereotype.Component;
+import javax.servlet.http.HttpServletRequest;
+import static org.springframework.http.HttpHeaders.AUTHORIZATION;
+import static org.springframework.http.HttpHeaders.CONTENT_TYPE;
+
+@Component @AllArgsConstructor
+public class ControllerHelper {
+    private AuthService authService;
+
+    public boolean hasAuthority(HttpServletRequest request, String scope) {
+        String authorizationHeader = request.getHeader(AUTHORIZATION);
+        return authService.hasAuthority(authorizationHeader, scope);
+    }
+
+    public HttpHeaders setupResponseHeaders(HttpServletRequest request) {
+        HttpHeaders headers = new HttpHeaders();
+        headers.set(CONTENT_TYPE, "application/json");
+        headers.set(AUTHORIZATION, request.getHeader(AUTHORIZATION));
+        headers.add("X-Prev-Path", request.getRequestURI());
+        headers.add("X-Request-Path", request.getHeader("X-Request-Path"));
+        headers.add("X-Request-Id", request.getHeader("X-Request-Id"));
+        return headers;
+    }
+}
