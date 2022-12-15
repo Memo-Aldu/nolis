c9c8fb65e749a6e1ee610c20def81133ebbae627
Memo aldu <memoaldu@Memos-MacBook-Pro-2.local>
2022-12-14 23:35:15 -0500

    Refactored Http Configuration and added file to .gitingore

--- nolisservices/productsearch/src/main/java/com/nolis/productsearch/configuration/HttpConfiguration.java
+++ nolisservices/productsearch/src/main/java/com/nolis/productsearch/configuration/HttpConfiguration.java
@@ -6,19 +6,23 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
 
-@Configuration
+@Configuration("httpConfigurationBean")
 public class HttpConfiguration {
 
     @Bean
     @LoadBalanced
     @Qualifier("loadBalancedRestTemplate")
     public RestTemplate restTemplate() {
-        return com.nolis.commonconfig.security.configuration.HttpConfiguration.customRestTemplate();
+        return getCustomResetTemplate();
     }
 
     @Bean
     @Qualifier("withoutLoadBalanced")
     public RestTemplate restTemplateWithoutLoadBalancing() {
+        return getCustomResetTemplate();
+    }
+
+    private RestTemplate getCustomResetTemplate() {
         return com.nolis.commonconfig.security.configuration.HttpConfiguration.customRestTemplate();
     }
 }
