50cd77007ae3053858e6c82ea49396fb8627a592
Memo- Aldu <71087733+Memo-Aldu@users.noreply.github.com>
2023-01-16 00:04:07 -0500

    Added topic constant

--- nolisservices/common-data/src/main/java/com/nolis/commondata/constants/Kafka.java
+++ nolisservices/common-data/src/main/java/com/nolis/commondata/constants/Kafka.java
@@ -2,5 +2,6 @@ package com.nolis.commondata.constants;
 
 public class Kafka {
     public static final String SEARCH_REGISTRY_TOPIC = "kafka-registry-search-topic";
+    public static final String DELETED_SEARCH_TOPIC = "kafka-deleted-search-topic";
 
 }
